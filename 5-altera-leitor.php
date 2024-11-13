659e34c294846a9e10c063856671b3ceab0ac105
Aline <106451792+LittleStaar@users.noreply.github.com>
2024-11-12 21:52:25 -0300

    Update 5-altera-leitor.php

--- 5-altera-leitor.php
+++ 5-altera-leitor.php
@@ -5,12 +5,10 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<header>
         <a href="index.html"><button title="Voltar ao início">Home</button></a>
         <h1 class="text-center">SISTEMA BIBLIOTECA</h1>
         <h3 class="text-center">CADASTRO DE LEITORES</h3>
         <hr>
-</header>
 <?php
 // Conexão com o banco de dados
 include "config.php";
@@ -20,28 +18,28 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $codleitor = $_POST['codleitor'];
-    $nome = $_POST['nome'];
-    $dtnasc = $_POST['dtnasc'];
-    $celular = $_POST['celular'];
-    $email = $_POST['email'];
-    $ra = $_POST['ra'];
-    $endereco = $_POST['endereco'];
-    $num_end = $_POST['num_end'];
-    $bairro = $_POST['bairro'];
-    $cidade = $_POST['cidade'];
+    $cCodLeitor = $_POST['CodLeitor'];
+    $Nome = $_POST['Nome'];
+    $DtNasc = $_POST['DtNasc'];
+    $Celular = $_POST['Celular'];
+    $Email = $_POST['Email'];
+    $RA = $_POST['RA'];
+    $Endereco = $_POST['Endereco'];
+    $NumEnd = $_POST['NumEnd'];
+    $Bairro = $_POST['Bairro'];
+    $CidadeUF = $_POST['CidadeUF'];
 
     $sql = "UPDATE leitores SET 
-                Nome='$nome', 
-                DtNasc='$dtnasc', 
-                Celular='$celular', 
-                Email='$email',
-                RA='$ra', 
-                Endereco='$endereco', 
-                NumEnd='$num_end', 
-                Bairro='$bairro', 
-                CidadeUF='$cidade' 
-            WHERE CodLeitor='$codleitor'";
+                Nome='$Nome', 
+                DtNasc='$DtNasc', 
+                Celular='$Celular', 
+                Email='$Email',
+                RA='$RA', 
+                Endereco='$Endereco', 
+                NumEnd='$NumEnd', 
+                Bairro='$Bairro', 
+                CidadeUF='$CidadeUF' 
+            WHERE CodLeitor='$CodLeitor'";
     
     $resultado = mysqli_query($conn, $sql);
 
